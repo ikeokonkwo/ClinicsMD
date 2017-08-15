79c7f786debd45f442bb5ee1f2b3d712dda5bfa4
Ike Okonkwo <ikeokonkwo@college.harvard.edu>
2017-08-15 01:25:08 -0300

    corrected repository

---
+++ Clinics_MD/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # get 'main/index'
+
+  get '/', to: 'main#home'
+
+
+
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
